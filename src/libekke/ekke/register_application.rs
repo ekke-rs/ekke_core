00ab2c6478c8856b51cfe0fdc53dffeb3809df76
Naja Melan <najamelan@autistici.org>
2019-02-14 05:10:06 +0100

    Oblige services to return a response from handler

--- src/libekke/ekke/register_application.rs
+++ src/libekke/ekke/register_application.rs
@@ -1,27 +1,49 @@
-use typename          :: { TypeName               };
-use actix             :: { prelude::*             };
-use serde_derive      :: { Serialize, Deserialize };
+use typename          :: { TypeName                        };
+use actix             :: { prelude::*                      };
+use serde_derive      :: { Serialize, Deserialize          };
+use ekke_io           :: { IpcMessage, ConnID, MessageType };
+use slog              :: { info                            };
 
 
 use crate::Ekke;
 
-#[ derive( Debug, Clone, Serialize, Deserialize, Message, TypeName ) ]
+#[ derive( Debug, Clone, Serialize, Deserialize, Message, TypeName ) ] #[rtype(result="IpcMessage")]
 //
 pub struct RegisterApplication
 {
-	pub app_name: String
+	pub app_name: String,
+	pub conn_id : ConnID,
+}
+
+#[ derive( Debug, Clone, Serialize, Deserialize, Message, TypeName ) ]
+//
+pub struct RegisterApplicationResponse
+{
+	pub response: String
 }
 
 
 
 impl Handler<RegisterApplication> for Ekke
 {
-	type Result = ();
+	type Result = IpcMessage;
 
 	fn handle( &mut self, msg: RegisterApplication, _ctx: &mut Context<Self> ) -> Self::Result
 	{
-		println!( "Ekke: Received app registration for app: {}", msg.app_name );
-	}
+		info!( self.log, "Ekke: Received app registration for app: {}", msg.app_name );
 
+		let service = "RegisterApplicationResponse".to_string();
+		let payload = RegisterApplicationResponse{ response: "Thank you for registering with Ekke".to_string() };
+		let ms_type = MessageType::Response;
+		let conn_id = msg.conn_id;
+
+		IpcMessage::new
+		(
+			  service
+			, payload
+			, ms_type
+			, conn_id
+		)
+	}
 }
 
